b9f1a22dc1988aa132853595f889d571d2c5faf8
Fedor Pushkov <f.pushkov@g.nsu.ru>
2019-12-20 04:44:25 +0700

    edited chat controller

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Login } from "./components/login/Login";
+import { Chat } from "./components/chat/Chat";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Login />
+        {/* <Chat /> */}
       </div>
     );
   }
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import "./Chat.css";
 
 export class Chat extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container-fluid  d-flex flex-column justify-content-center align-items-center chat-background">
+        <div className="container chat-container">
+          <div className="row">
+            <div className="col-4">AAAA</div>
+            <div className="col-8">BBBB</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
