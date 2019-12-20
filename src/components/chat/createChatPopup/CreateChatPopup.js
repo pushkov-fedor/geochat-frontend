ff454e4647ff595189c06e11e3421687cde9a455
Fedor Pushkov <f.pushkov@g.nsu.ru>
2019-12-21 01:21:54 +0700

    logic for creating chat

--- src/components/chat/createChatPopup/CreateChatPopup.js
+++ src/components/chat/createChatPopup/CreateChatPopup.js
@@ -2,15 +2,40 @@ import React, { Component } from "react";
 import "./CreateChatPopup.css";
 
 export class CreateChatPopup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chatTitle: ""
+    };
+
+    this.handleChatTitleChange = this.handleChatTitleChange.bind(this);
+    this.submitCreateChat = this.submitCreateChat.bind(this);
+  }
+
+  handleChatTitleChange(e) {
+    this.setState({ chatTitle: e.target.value });
+  }
+
+  submitCreateChat(e) {
+    e.preventDefault();
+  }
+
   render() {
+    console.log(this.state.chatTitle);
     return (
       <div className="create-chat-popup d-flex flex-column justify-content-center align-items-center">
-        <form className="d-flex flex-column justify-content-center align-items-center">
+        <form
+          className="d-flex flex-column justify-content-center align-items-center"
+          onSubmit={this.submitCreateChat}
+        >
           <div className="form-group d-flex flex-column justify-content-center align-items-center">
             <input
               type="text"
               className="form-control"
               id="inputChatTitle"
+              value={this.state.chatTitle}
+              onChange={this.handleChatTitleChange}
               placeholder="Enter chat name"
             />
           </div>
