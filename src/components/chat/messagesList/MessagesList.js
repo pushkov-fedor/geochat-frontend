69f1d83f80d2d688a4b03283abdf2aafc3613792
Fedor Pushkov <f.pushkov@g.nsu.ru>
2019-12-21 03:35:01 +0700

    working on user input components

--- src/components/chat/messagesList/MessagesList.js
+++ src/components/chat/messagesList/MessagesList.js
@@ -5,8 +5,21 @@ export class MessagesList extends Component {
   render() {
     return (
       <div className="messages-list-background">
-        <div className="messages-list-block"></div>
-        <div className="user-input-block"></div>
+        <div className="messages-list-inner-block"></div>
+        <div className="user-input-block">
+          <form className="user-input-message-form d-flex flex-direction-row align-items-center justify-content-start">
+            <div className="user-input-message-wrapper d-flex flex-direction-row align-items-center justify-content-center">
+              <input
+                type="text"
+                className="user-input-message"
+                placeholder="Type something and send"
+              ></input>
+            </div>
+            <div className="user-input-send-button-wrapper d-flex flex-direction-row align-items-center justify-content-center">
+              <button type="submit" className="user-input-send-button"></button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
