f4b267680f51678b69bf1b1a8d500f21ffb86e2c
Fedor Pushkov <f.pushkov@g.nsu.ru>
2019-12-20 21:54:58 +0700

    added chat shadow

--- src/components/chat/dialogList/dialog/Dialog.js
+++ src/components/chat/dialogList/dialog/Dialog.js
@@ -7,20 +7,6 @@ export class Dialog extends Component {
   }
 
   render() {
-    return (
-      <div className="dialog-container d-flex align-items-center justify-content-center">
-        <div className="dialog-card d-flex flex-column justify-content-start">
-          <div className="title-pane d-flex flex-column justify-content-center">
-            <h1 className="title">{this.props.title}</h1>
-          </div>
-          <div className="message-pane d-flex">
-            <p className="align-self-center message">
-              <span className="author-dark">{this.props.author}:</span>{" "}
-              {this.props.message}
-            </p>
-          </div>
-        </div>
-      </div>
-    );
+    return <div className=""></div>;
   }
 }
